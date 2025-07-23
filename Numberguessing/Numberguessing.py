f03ba342de5df0c299771a7ed8ae6bfb244487c1
NoJamesHere <davidedwin2007@gmail.com>
2025-07-23 20:39:02 +0200

    Had some bug fixes

--- Numberguessing/Numberguessing.py
+++ Numberguessing/Numberguessing.py
@@ -6,12 +6,23 @@ trys = 0
 print("Guess the number! Ranging from 1 to 100!")
 while running:
     command = input()
-    if(int(command) == number):
-        print(f"CORRECT! The number you guessed was {number}, CONGRATS!\nYour number of guesses:{trys}")
-        running = False
-    elif(int(command) > number):
+
+    if not command.isdigit():
+        print("Please enter an actual number..")
+        continue
+    guess = int(command)
+    if (guess < 1 or guess > 100):
+        print("The number to guess ranges from 100 to 1, try again..")
+        continue
+    if(guess == number):
+        if(trys == 0):
+            print(f"WOW! YOU GUESSED THE NUMBER {number} ON FIRST TRY!")
+        else:
+            print(f"CORRECT! The number you guessed was {number}, CONGRATS!\nYour number of guesses:{trys}")
+            running = False
+    elif(guess > number):
         print("Lower")
         trys += 1
-    elif(int(command) < number):
+    elif(guess < number):
         print("Higher")
         trys += 1
