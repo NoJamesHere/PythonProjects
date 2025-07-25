3d1cdbbd7632a5d9de7b85f389ac8bfe1313f1be
NoJamesHere <davidedwin2007@gmail.com>
2025-07-25 17:22:53 +0200

    Explored Flask! Pretty cool stuff

---
+++ Flaskquestionmark/backend.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, jsonify
+import base64
+
+app = Flask(__name__)
+
+themessage = ''
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/api/echo', methods=['POST'])
+def echo():
+    global themessage
+    data = request.get_json()
+    themessage = data.get('msg', '')
+    return jsonify({"msg": themessage})
+
+@app.route('/api/greet')
+def greet():
+    global themessage
+    encrypt = base64.b64encode(themessage.encode()).decode()
+    return jsonify({"message": f"Decrypted: {encrypt}"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
